e4ee78aef9e7d280cb0764e1231f7d6477eeff30
Nathaniel Tampus <nate@mugnavo.com>
2024-10-07 15:15:16 +0800

    disable multi-word-component-names eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,16 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
   // Your custom configs here
-)
+  {
+    rules: {
+      "vue/multi-word-component-names": [
+        "off", // disabled for now
+        // {
+        //   ignores: ["default"],
+        // },
+      ],
+    },
+  },
+);
