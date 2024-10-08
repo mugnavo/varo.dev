f023f019e47583fb6412f8c67a18cff00947a317
Nathaniel Tampus <nate@mugnavo.com>
2024-10-08 19:58:36 +0800

    enable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-10-07",
-  // devtools: { enabled: true },
+  devtools: { enabled: true },
 
   future: {
     compatibilityVersion: 4,
