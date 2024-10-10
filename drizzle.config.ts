d2c21a7bc6d88b0a049705fbc6ba8331a0db00dc
Nathaniel Tampus <nate@mugnavo.com>
2024-10-10 13:08:39 +0800

    move drizzle kit folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  out: "./drizzle",
+  out: "./.drizzle",
   schema: "./server/schemas/*.ts",
   breakpoints: true,
   dialect: "postgresql",
