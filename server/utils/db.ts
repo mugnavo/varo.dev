6a41dd89d478f88c5bfbe7e87a56be1cc94e511d
Nathaniel Tampus <nate@mugnavo.com>
2024-10-11 18:22:26 +0800

    add runtime error when db url is not set

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -5,6 +5,10 @@ import * as schema from "../schemas";
 
 const runtimeConfig = useRuntimeConfig();
 
+if (!runtimeConfig.databaseUrl) {
+  throw new Error("Missing NUXT_DATABASE_URL in env");
+}
+
 const driver = postgres(runtimeConfig.databaseUrl);
 
 export const db = drizzle(driver, { schema });
