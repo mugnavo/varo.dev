071b72c8c859df9a6a4cf3e87b9f18c6bdd43ad0
Nathaniel Tampus <nate@mugnavo.com>
2024-10-12 11:45:11 +0800

    remove nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,39 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-10-07",
-  devtools: { enabled: true },
+	compatibilityDate: "2024-10-07",
+	devtools: { enabled: true },
 
-  future: {
-    compatibilityVersion: 4,
-  },
+	future: {
+		compatibilityVersion: 4,
+	},
 
-  modules: [
-    "@pinia/nuxt",
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@vueuse/nuxt",
-    "@nuxthub/core",
-    "@nuxtjs/tailwindcss",
-    "nuxt-auth-utils",
-    "nuxt-typed-router",
-    "nuxt-build-cache",
-    "@vant/nuxt",
-  ],
+	modules: [
+		"@pinia/nuxt",
+		"@nuxt/eslint",
+		"@nuxt/fonts",
+		"@vueuse/nuxt",
+		"@nuxtjs/tailwindcss",
+		"nuxt-auth-utils",
+		"nuxt-typed-router",
+		"nuxt-build-cache",
+		"@vant/nuxt",
+	],
 
-  runtimeConfig: {
-    databaseUrl: process.env.NUXT_DATABASE_URL,
+	runtimeConfig: {
+		databaseUrl: process.env.NUXT_DATABASE_URL,
 
-    sessionPassword: process.env.NUXT_SESSION_PASSWORD,
-    baseUrl: process.env.NUXT_BASE_URL,
+		sessionPassword: process.env.NUXT_SESSION_PASSWORD,
+		baseUrl: process.env.NUXT_BASE_URL,
 
-    googleGenerativeAiApiKey: process.env.NUXT_GOOGLE_GENERATIVE_AI_API_KEY,
+		googleGenerativeAiApiKey: process.env.NUXT_GOOGLE_GENERATIVE_AI_API_KEY,
 
-    oauth: {
-      github: {
-        clientId: process.env.NUXT_GITHUB_CLIENT_ID,
-        clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
-        redirectURL: process.env.NUXT_BASE_URL! + process.env.NUXT_GITHUB_REDIRECT_PATH,
-      },
-    },
-  },
+		oauth: {
+			github: {
+				clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+				clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
+				redirectURL:
+					process.env.NUXT_BASE_URL! + process.env.NUXT_GITHUB_REDIRECT_PATH,
+			},
+		},
+	},
 });
