c9f0e99448a1e7bf2e2fec9f6dd636fd44a9903a
Peroxidize <marionemmanuelbuhat@gmail.com>
2024-10-25 16:02:36 +0800

    updated landing page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,22 +2,10 @@
 const { user, clear } = useUserSession();
 </script>
 <template>
-	<div class="flex flex-col gap-4 p-6">
-		<h1 class="text-4xl font-bold">varo.dev</h1>
-
-		<div v-if="user" class="flex flex-col gap-2">
-			<p>Welcome back, {{ user.name }}!</p>
-			<van-button type="success" class="w-fit" to="/app">Go to App</van-button>
-			<div>
-				More data:
-				<pre>{{ JSON.stringify(user, null, 2) }}</pre>
-			</div>
-
-			<van-button type="danger" class="w-fit" @click="clear"> Sign out </van-button>
-		</div>
-		<div v-else class="flex flex-col gap-2">
-			<p>You are not signed in.</p>
-			<van-button type="primary" class="w-fit" to="/signin">Sign in</van-button>
+	<div class="h-full w-full">
+		<div class="h-full flex justify-center items-center flex-col gap-4">
+			<p class="text-5xl font-medium">Varo.dev</p>
+			<p class="text-xl">A matchmaking platform for developers, open-source projects, and indie founders.</p>
 		</div>
 	</div>
 </template>
