04986ef5e065e0da59eed29b65e772e0297aa9d6
Peroxidize <marionemmanuelbuhat@gmail.com>
2024-10-25 16:06:42 +0800

    changed index page header font weight

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@ const { user, clear } = useUserSession();
 <template>
 	<div class="h-full w-full">
 		<div class="h-full flex justify-center items-center flex-col gap-4">
-			<p class="text-5xl font-medium">Varo.dev</p>
+			<p class="text-5xl font-bold">Varo.dev</p>
 			<p class="text-xl">A matchmaking platform for developers, open-source projects, and indie founders.</p>
 		</div>
 	</div>
