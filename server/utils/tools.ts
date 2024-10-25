e4baf5c1356e70e3d05ae5c324def7d18e4e1ac1
Nathaniel Tampus <nate@mugnavo.com>
2024-10-25 19:58:02 +0800

    don't include self when searching for users

--- server/utils/tools.ts
+++ server/utils/tools.ts
@@ -1,79 +1,84 @@
+import type { User } from "#auth-utils";
 import { tool } from "ai";
-import { and, cosineDistance, eq, gt, sql } from "drizzle-orm";
+import { and, cosineDistance, eq, gt, ne, sql } from "drizzle-orm";
 import { z } from "zod";
 import { safeAwait } from "~~/layers/josh/utils/safeTry";
 import { userEmbeddings, users } from "../schemas";
 import { generateEmbedding } from "./embedding";
 
-export const searchDevelopers = tool({
-	description: `Search for developers in the platform based on the user's prompt.`,
-	parameters: z.object({
-		query: z.string().describe("The user's search query."),
-	}),
+export function getTools(user: User) {
+	const searchDevelopers = tool({
+		description: `Search for developers in the platform based on the user's prompt.`,
+		parameters: z.object({
+			query: z.string().describe("The user's search query."),
+		}),
 
-	execute: async (params) => {
-		console.log("calling searchDevelopers");
-		console.log(params);
-		const embeddedQuery = await generateEmbedding(params.query);
+		execute: async (params) => {
+			console.log("calling searchDevelopers");
+			console.log(params);
+			const embeddedQuery = await generateEmbedding(params.query);
 
-		const userSimilarity = sql<number>`1 - (${cosineDistance(
-			userEmbeddings.embedding,
-			embeddedQuery,
-		)})`;
+			const userSimilarity = sql<number>`1 - (${cosineDistance(
+				userEmbeddings.embedding,
+				embeddedQuery,
+			)})`;
 
-		console.log(userSimilarity);
+			const [_err, matchUsers] = await safeAwait(
+				db
+					.selectDistinctOn([userEmbeddings.user_id], {
+						embedding_similarity: userSimilarity,
+						embedding_content: userEmbeddings.content,
+						user_id: userEmbeddings.user_id,
+						user_name: users.name,
+						user_avatar: users.avatar_url,
+						user_location: users.location,
+						user_bio: users.bio,
+						user_username: users.username,
+						user_skills: users.skills,
+						user_tech_stack: users.tech_stack,
+						user_interests: users.interests,
+						user_availability: users.availability,
+						user_experience: users.experience_level,
+						user_match_user: users.match_user,
+						user_match_project: users.match_project,
+					})
+					.from(userEmbeddings)
+					.innerJoin(users, eq(userEmbeddings.user_id, users.id))
+					.where(
+						and(
+							gt(userSimilarity, 0.6),
+							ne(userEmbeddings.user_id, user.id),
+							eq(users.match_user, true),
+						),
+					)
+					.limit(10),
+			);
+			console.log(matchUsers);
 
-		const [_err, matchUsers] = await safeAwait(
-			db
-				.selectDistinctOn([userEmbeddings.user_id], {
-					embedding_similarity: userSimilarity,
-					embedding_content: userEmbeddings.content,
-					user_id: userEmbeddings.user_id,
-					user_name: users.name,
-					user_avatar: users.avatar_url,
-					user_location: users.location,
-					user_bio: users.bio,
-					user_username: users.username,
-					user_skills: users.skills,
-					user_tech_stack: users.tech_stack,
-					user_interests: users.interests,
-					user_availability: users.availability,
-					user_experience: users.experience_level,
-					user_match_user: users.match_user,
-					user_match_project: users.match_project,
-				})
-				.from(userEmbeddings)
-				.innerJoin(users, eq(userEmbeddings.user_id, users.id))
-				.where(
-					and(
-						gt(userSimilarity, 0.6),
-						// ne(userEmbeddings.user_id, user.id),
-						eq(users.match_user, true),
-					),
-				)
-				.limit(10),
-		);
-		console.log(matchUsers);
+			return { component: "DeveloperList" as const, developers: matchUsers };
+		},
+	});
 
-		return { component: "DeveloperList" as const, developers: matchUsers };
-	},
-});
+	const searchProjects = tool({
+		description: `Search for projects in the platform based on the user's prompt.`,
+		parameters: z.object({
+			query: z.string().describe("The user's search query."),
+		}),
 
-export const searchProjects = tool({
-	description: `Search for projects in the platform based on the user's prompt.`,
-	parameters: z.object({
-		query: z.string().describe("The user's search query."),
-	}),
+		execute: async (params) => {
+			console.log("calling searchProjects");
+			console.log(params);
 
-	execute: async (params) => {
-		console.log("calling searchProjects");
-		console.log(params);
+			return { component: "ProjectList" as const, props: params };
+		},
+	});
 
-		return { component: "ProjectList" as const, props: params };
-	},
-});
+	return { searchDevelopers, searchProjects };
+}
 
 export type SearchDevelopersReturnType = Awaited<
-	ReturnType<typeof searchDevelopers.execute>
+	ReturnType<ReturnType<typeof getTools>["searchDevelopers"]["execute"]>
 >;
-export type SearchProjectsReturnType = Awaited<ReturnType<typeof searchProjects.execute>>;
+export type SearchProjectsReturnType = Awaited<
+	ReturnType<ReturnType<typeof getTools>["searchProjects"]["execute"]>
+>;
