9bda090268770100aad75d173aaf7442ec77e741
Nathaniel Tampus <nate@mugnavo.com>
2024-10-25 20:54:17 +0800

    update landing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,9 +7,12 @@ const { user, clear } = useUserSession();
 </script>
 <template>
 	<div class="h-full w-full">
-		<div class="h-full flex justify-center items-center flex-col gap-4">
-			<p class="text-5xl font-bold">Varo.dev</p>
-			<p class="text-xl">A matchmaking platform for developers, open-source projects, and indie founders.</p>
+		<div class="flex h-full flex-col items-center justify-center gap-4">
+			<p class="text-5xl font-bold">Varo</p>
+			<p class="text-xl">
+				An experimental matchmaking platform for developers, open-source projects,
+				and indie founders.
+			</p>
 		</div>
 	</div>
 </template>
