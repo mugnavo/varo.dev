6e6d1906f078e577b7504a866b31aecd9470f9e1
Nathaniel Tampus <nate@mugnavo.com>
2025-01-11 17:39:11 +0800

    fix signin page

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -3,6 +3,7 @@ import { Discord } from "~/lib/components/icons/Discord";
 import { Github } from "~/lib/components/icons/Github";
 import { Google } from "~/lib/components/icons/Google";
 import { Button } from "~/lib/components/ui/button";
+import authClient from "~/lib/utils/auth-client";
 
 export const Route = createFileRoute("/signin")({
   component: AuthPage,
@@ -24,11 +25,13 @@ function AuthPage() {
     <div className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-8 rounded-xl border bg-muted p-10">
         Varo
-        <form method="GET" className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
           <Button
             className="bg-[#5865F2] text-white hover:bg-[#5865F2]/80"
-            formAction="/api/auth/discord"
-            type="submit"
+            onClick={() => {
+              authClient.signIn.social({ provider: "discord", callbackURL: "/app" });
+            }}
+            type="button"
             size="lg"
           >
             <Discord className="mr-2 size-4" />
@@ -36,8 +39,10 @@ function AuthPage() {
           </Button>
           <Button
             className="bg-[#333333] text-white hover:bg-[#333333]/80"
-            formAction="/api/auth/github"
-            type="submit"
+            onClick={() => {
+              authClient.signIn.social({ provider: "github", callbackURL: "/app" });
+            }}
+            type="button"
             size="lg"
           >
             <Github className="mr-2 size-4" />
@@ -45,14 +50,16 @@ function AuthPage() {
           </Button>
           <Button
             className="bg-[#DB4437] text-white hover:bg-[#DB4437]/80"
-            formAction="/api/auth/google"
-            type="submit"
+            onClick={() => {
+              authClient.signIn.social({ provider: "google", callbackURL: "/app" });
+            }}
+            type="button"
             size="lg"
           >
             <Google className="mr-2 size-4" />
             Login with Google
           </Button>
-        </form>
+        </div>
       </div>
     </div>
   );
